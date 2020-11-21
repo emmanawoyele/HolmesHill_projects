167389372c4c30b9f27506036ccfe9dc64ee302e
Manny <emmanawoyele@gmail.com>
2020-11-21 11:12:06 +0000

    add holmes_final

---
+++ Assets/js/portfolio.js
@@ -0,0 +1,45 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSliders(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+ 
+    showSliders(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlider(n) {
+    showSliders(slideIndex = n);
+  }
+  
+  function showSliders(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
